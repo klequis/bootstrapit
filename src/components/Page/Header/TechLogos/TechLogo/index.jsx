21f186c4c3da64bbb8c67a38d02edd055190f2d4
klequis <klequis@gmail.com>
2018-01-09 20:21:33 -0800

    tech logo w bootstrap

--- src/components/Page/Header/TechLogos/TechLogo/index.jsx
+++ src/components/Page/Header/TechLogos/TechLogo/index.jsx
@@ -2,7 +2,34 @@
 
 import React from 'react';
 import styles from './style.css';
+import { Grid, Row, Col } from 'react-bootstrap'
 
+const TechLogo = (props) => {
+  const imgStyle = {
+    maxHeight: props.maxHeight,
+    margin: 'auto',
+    display: 'block',
+    height: 'auto',
+    maxWidth: '100%'
+  }
+  return (
+    <Grid>
+      <Row>
+        <Col md={12}>
+          <img style={imgStyle} src={props.url} alt="logo" />
+          <div className={styles.logoText}>{props.name}</div>
+        </Col>
+      </Row>
+    </Grid>
+    
+
+  );
+};
+
+export default TechLogo;
+
+
+/*
 const TechLogo = (props) => {
   const imgStyle = {
     maxHeight: props.maxHeight,
@@ -19,5 +46,4 @@ const TechLogo = (props) => {
 
   );
 };
-
-export default TechLogo;
+*/
