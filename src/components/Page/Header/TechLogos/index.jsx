3dbdc53b344b37733a35d2f0292fac4521409117
klequis <klequis@gmail.com>
2018-01-09 21:27:20 -0800

    tech logo w bootstrap

--- src/components/Page/Header/TechLogos/index.jsx
+++ src/components/Page/Header/TechLogos/index.jsx
@@ -1,45 +1,37 @@
 // TechLogos
 import React from 'react';
-import { Row, Col } from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 import classNames from 'classnames'
 import styles from './style.css';
 import TechLogo from './TechLogo';
-import { logoRow1, logoRow2 } from './logo-data'
+import {logoRow1, logoRow2} from './logo-data'
 
 const TechLogos = (props) => {
 
-  const logoRowStyle = classNames({
-    'd-flex': true,
-    'flex-row': true,
-    'justify-content-around': true,
-  })
+  const logoRowStyle = classNames({'d-flex': true, 'flex-row': true, 'justify-content-around': true})
   const logoImgStyle = classNames({
     'img-fluid': true,
-    [styles.logoImgFix]: true,
+    [styles.logoImgFix]: true
   })
 
-  const renderLogoRow1 = logoRow1.map((t) => (
-    <TechLogo
-      key={t.name}
-      name={t.name}
-      url={t.image}
-      style={t.style}
-      maxWidth={t.maxWidth}
-      maxHeight={t.maxHeight}
-    />
-  ))
-  const renderLogoRow2 = logoRow2.map((t) => (
-    <TechLogo
-      key={t.name}
-      url={t.image}
-      style={t.style}
-      maxWidth={t.maxWidth}
-      maxHeight={t.maxHeight}
-    />
-  ))
+  const renderLogoRow1 = logoRow1.map((t) => (<TechLogo key={t.name} name={t.name} url={logoRow1[0].image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
+  const renderLogoRow2 = logoRow2.map((t) => (<TechLogo key={t.name} url={t.image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
 
-  return (
-    <div>
+  return (<div>
+    <div className={styles.logoRow1}>
+      {renderLogoRow1}
+    </div>
+    <div className={styles.logoRow2}>
+      {renderLogoRow2}
+    </div>
+  </div>)
+}
+
+export default TechLogos;
+
+/*
+      <div>
+
       <Row >
         <Col md={2}>
           <img src={logoRow1[0].image} className={logoImgStyle} />
@@ -72,19 +64,4 @@ const TechLogos = (props) => {
         </Col>
       </Row>
     </div>
-  )
-}
-
-export default TechLogos;
-
-/*
-<div>
-  <div className={styles.logoRow1}>
-    {renderLogoRow1}
-  </div>
-  <div className={styles.logoRow2}>
-    {renderLogoRow2}
-  </div>
-</div>
-
 */
