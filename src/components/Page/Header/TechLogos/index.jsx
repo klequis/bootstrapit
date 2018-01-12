06ee066cf1b0d37d725dc04f0c4e16018f5b0853
klequis <klequis@gmail.com>
2018-01-11 19:23:33 -0800

    Created Card to replace TechLogo

--- src/components/Page/Header/TechLogos/index.jsx
+++ src/components/Page/Header/TechLogos/index.jsx
@@ -1,9 +1,6 @@
-// TechLogos
 import React from 'react';
-import {Row, Col} from 'react-bootstrap'
 import classNames from 'classnames'
-import styles from './style.css';
-import TechLogo from './TechLogo';
+import Card from '../../../elements/Card'
 import {logoRow1, logoRow2} from './logo-data'
 
 const TechLogos = (props) => {
@@ -14,13 +11,9 @@ const TechLogos = (props) => {
     'flex-wrap': true,
     'justify-content-around': true
   })
-  const logoImgStyle = classNames({
-    'img-fluid': true,
-    [styles.logoImgFix]: true
-  })
 
-  const renderLogoRow1 = logoRow1.map((t) => (<TechLogo key={t.name} name={t.name} url={logoRow1[0].image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
-  const renderLogoRow2 = logoRow2.map((t) => (<TechLogo key={t.name} url={t.image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
+  const renderLogoRow1 = logoRow1.map((t) => (<Card key={t.name} name={t.name} url={t.image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
+  const renderLogoRow2 = logoRow2.map((t) => (<Card key={t.name} url={t.image} style={t.style} maxWidth={t.maxWidth} maxHeight={t.maxHeight}/>))
 
   return (<div>
     <div className={logoRowStyle}>
@@ -33,40 +26,3 @@ const TechLogos = (props) => {
 }
 
 export default TechLogos;
-
-/*
-      <div>
-
-      <Row >
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-        <Col md={2}>
-          <img src={logoRow1[0].image} className={logoImgStyle} />
-          <p>Text</p>
-        </Col>
-      </Row>
-      <Row >
-        <Col md={12} className={logoRowStyle}>
-          {renderLogoRow2}
-        </Col>
-      </Row>
-    </div>
-*/
